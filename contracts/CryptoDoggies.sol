fd214a2347eaf6209d5042469555fc2af81ec59d
Rory Murray <rory.christian.murray@gmail.com>
2020-02-14 21:11:50 -0500

    Data types

--- contracts/CryptoDoggies.sol
+++ contracts/CryptoDoggies.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.4.18;
 
 contract CryptoDoggies {
+    uint age = 10;
+
+    string name = 'Rory'
+
+    uint8 = 2 // 8 bit int
+    uint256 = 18282 // goes up to 256
+    bytes5 = 'stores 5 bytes of information'
 
 }
