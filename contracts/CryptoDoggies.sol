954f560c1c32933ac19ef18b9ec395830b11724a
Rory Murray <rory.christian.murray@gmail.com>
2020-02-14 22:44:19 -0500

    Create Doggy struct;

--- contracts/CryptoDoggies.sol
+++ contracts/CryptoDoggies.sol
@@ -2,11 +2,34 @@ pragma solidity ^0.4.18;
 
 contract CryptoDoggies {
     uint age = 10;
+    uint maxAge = age * 5;
 
     string name = 'Rory'
+    string name2 = 'Amy'
 
     uint8 = 2 // 8 bit int
     uint256 = 18282 // goes up to 256
-    bytes5 = 'stores 5 bytes of information'
+    bytes3 = 'stores 3 bytes of information'
+
+    // structs are similar to classes
+
+    struct Doggy {
+        uint age;
+        string name;
+        bytes5 dna;
+    }
+
+    Doggy doggy1 = Doggy({
+        age: age,
+        name: name,
+        dna: bytes5(0x000000000)
+    })
+    
+
+    Doggy doggy1 = Doggy({
+        age: m,
+        name: name,
+        dna: bytes5(0x000000000)
+    })
 
 }
